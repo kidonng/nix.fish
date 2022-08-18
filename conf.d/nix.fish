19cfe6c7f1e8ae60865b22197fc43506d78888f8
Kid <hi@xuann.wang>
2022-08-18 21:17:39 +0800

    Use long options

--- conf.d/nix.fish
+++ conf.d/nix.fish
@@ -1,34 +1,34 @@
-set -l profile ~/.nix-profile
-set -l default /nix/var/nix/profiles/default
+set --local profile ~/.nix-profile
+set --local default /nix/var/nix/profiles/default
 
-set -l channels ~/.nix-defexpr/channels
-contains $channels $NIX_PATH || set -agx NIX_PATH $channels
+set --local channels ~/.nix-defexpr/channels
+contains $channels $NIX_PATH || set --global --export --append NIX_PATH $channels
 
-function _nix_install -e nix_install -V profile
-    set -Ux NIX_PROFILES $profile
+function _nix_install --on-event nix_install --inherit-variable profile
+    set --universal --export NIX_PROFILES $profile
 
     if test -d $default && ! contains $default $NIX_PROFILES
-        set -p NIX_PROFILES $default
+        set --prepend NIX_PROFILES $default
     end
 
     # Also check for /etc/ssl/cert.pem, see https://github.com/NixOS/nix/issues/5461
     for file in /etc/{ssl/{certs/ca-certificates.crt,ca-bundle.pem,certs/ca-bundle.crt},pki/tls/certs/ca-bundle.crt,ssl/cert.pem} $profile/etc/{ssl/certs/,}ca-bundle.crt
         if test -e $file
-            set -Ux NIX_SSL_CERT_FILE $file
+            set --universal --export NIX_SSL_CERT_FILE $file
             break
         end
     end
 end
 
-function _nix_uninstall -e nix_uninstall
-    set -e NIX_{PATH,PROFILES,SSL_CERT_FILE}
-    functions -e _nix_{,un}install
+function _nix_uninstall --on-event nix_uninstall
+    set --erase NIX_{PATH,PROFILES,SSL_CERT_FILE}
+    functions --erase _nix_{,un}install
 end
 
-test -n "$MANPATH" && set -p MANPATH $profile/share/man
+test -n "$MANPATH" && set --prepend MANPATH $profile/share/man
 
-set -l packages (string match -r "/nix/store/[\w.-]+" $PATH)
-fish_add_path -ag $packages/bin $profile/bin $default/bin
+set --local packages (string match --regex "/nix/store/[\w.-]+" $PATH)
+fish_add_path --global --append $packages/bin $profile/bin $default/bin
 
 if test (count $packages) != 0
     set fish_complete_path $fish_complete_path[1] \
@@ -41,7 +41,7 @@ if test (count $packages) != 0
         $fish_function_path[2..]
 
     for file in $packages/etc/fish/conf.d/*.fish $packages/share/fish/vendor_conf.d/*.fish
-        if ! test -f (string replace -r "^.*/" $__fish_config_dir/conf.d/ -- $file)
+        if ! test -f (string replace --regex "^.*/" $__fish_config_dir/conf.d/ -- $file)
             source $file
         end
     end
